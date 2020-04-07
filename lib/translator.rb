556a4357dd2266b082fa2c35afdaf11025ec6d47
Marshall Slemp <marshall.slemp@gmail.com>
2020-04-07 08:16:52 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -16,13 +16,34 @@ end
 
 def get_japanese_emoticon(file_path, emoticon)
   # code goes here
-  hash = load_library(file_path)
+  emoticons = load_library(file_path)
   
-  hash.each do |key, value|
-    
+  emoticons.each do |outer_key, emoticon_hash|
+    inner_hash.each do |inner_key, inner_emoticon|
+      if inner_emoticon == emoticon
+        return emoticons[outer_key][:japanese]
+      end
+    end
   end
 end
 
 def get_english_meaning
   # code goes here
 end
+
+
+{
+  "angel"=>{:english=>"O:)", :japanese=>"☜(⌒▽⌒)☞"}, 
+  "angry"=>{:english=>">:(", :japanese=>"ヽ(ｏ`皿′ｏ)ﾉ"}, 
+  "bored"=>{:english=>":O", :japanese=>"(ΘεΘ;)"}, 
+  "confused"=>{:english=>"%)", :japanese=>"(゜.゜)"}, 
+  "embarrassed"=>{:english=>":$", :japanese=>"(#^.^#)"}, 
+  "fish"=>{:english=>"><>", :japanese=>">゜))))彡"}, 
+  "glasses"=>{:english=>"8D", :japanese=>"(^0_0^)"}, 
+  "grinning"=>{:english=>"=D", :japanese=>"(￣ー￣)"}, 
+  "happy"=>{:english=>":)", :japanese=>"(＾ｖ＾)"}, 
+  "kiss"=>{:english=>":*", :japanese=>"(*^3^)/~☆"}, 
+  "sad"=>{:english=>":'(", :japanese=>"(Ｔ▽Ｔ)"}, 
+  "surprised"=>{:english=>":o", :japanese=>"o_O"}, 
+  "wink"=>{:english=>";)", :japanese=>"(^_-)"}
+}
